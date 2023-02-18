c354984e0539c97202e23330047267026731738d
Sravanasaikrishna1996 <Sravanasaikrishna@outlook.com>
2023-02-18 11:46:11 +0530

    Sample program to Test

---
+++ hello.c
@@ -0,0 +1,28 @@
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////This is the example program for to practice in GIT
+//
+//
+//
+//
+//
+//
+//
+//
+//
+#include<stdio.h>
+int main()
+{
+	printf("Hello World");
+
+	printf("Hello Git Hub);
+
+	printf("Basic Example program in GIT");
+}
